73254306e99b0f5fd21df7e5e303c5f52f0f9668
DESKTOP-NVBDOD4\Fujitsu <jystineangelmanatad@gmail.com>
2024-05-09 15:56:05 +0800

    add

--- index.js
+++ index.js
@@ -24,9 +24,10 @@ app.get('/portfolio', (req, res) => {
             { company: "PixelPerfect Designs", designation: "Junior UI Designer", details: "Assisted in the design and development of user interfaces for web and mobile applications. Worked closely with senior designers to translate concepts into visually appealing designs. Participated in brainstorming sessions and design critiques to improve design quality.", year: "2021-2023" }
         ],
         education: [
-            { school: "University of Southern Philippines Foundation Lahug", year: "kalimot ko" },
+            { school: "University of Southern Philippines Foundation Lahug", year: "Senior High School" },
             { school: "University of Southern Philippines Foundation Lahug", year: "2019-2021" },
-            { school: "University of Southern Philippines Foundation Lahug", year: "Year" }
+            { school: "University of Southern Philippines Foundation Lahug", year: "Junior High School" },
+            { school: "IGLC Montessori School Inc", year: "Elementary School" }
         ],
         personalReferences: [
             { name: "nice ka sir", relationship: "Supervisor at DesignLab Solutions", contactNo: "0923-456-7890" },
