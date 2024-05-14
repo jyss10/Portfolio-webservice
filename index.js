018ae9d033c5867137b97f9ffe19b93e4e15fc2c
CCSLAB3-PC24\USER <Jystineangelmanatad@gmail.com>
2024-05-14 13:38:23 +0800

    ADD

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.get('/portfolio', (req, res) => {
         personalInfo: {
             Name: "Jystine Angel Manatad",
             Birthday: "1998-12-25",
-            Gender: "F",
+            Gender: "Female",
             ContactNo: "ngeeeeeeeeeeee",
             Address: "secret imo pa lng ko kidnapon",
         },
@@ -21,13 +21,15 @@ app.get('/portfolio', (req, res) => {
         ],
         workExperience: [
             { company: "DesignLab Solution", designation: "UI/UX Designer", details: "Designed user interfaces and interactions for various digital products, focusing on usability and accessibility. Created wireframes, mockups, and interactive prototypes to communicate design ideas. Conducted usability testing and gathered feedback to refine designs.", year: "2019-2021"},
-            { company: "PixelPerfect Designs", designation: "Junior UI Designer", details: "Assisted in the design and development of user interfaces for web and mobile applications. Worked closely with senior designers to translate concepts into visually appealing designs. Participated in brainstorming sessions and design critiques to improve design quality.", year: "2021-2023" }
+            { company: "PixelPerfect Designs", designation: "Junior UI Designer", details: "Assisted in the design and development of user interfaces for web and mobile applications. Worked closely with senior designers to translate concepts into visually appealing designs. Participated in brainstorming sessions and design critiques to improve design quality.", year: "2021-2023" },
+            { company: "ACCENTURE", designation: "TIG PRINT", details: "KAKAPOY.", year: "2023-2024" }
         ],
         education: [
             { school: "University of Southern Philippines Foundation Lahug", year: "2022-present" },
             { school: "University of Southern Philippines Foundation Lahug", year: "2019-2021" },
             { school: "University of Southern Philippines Foundation Lahug", year: "2013-2017" },
-            { school: "IGLC Montessori School Inc", year: "20XX-20XX" }
+            { school: "IGLC Montessori School Inc", year: "20XX-20XX" },
+            { school: "UC", year: "2020-2021" }
         ],
         personalReferences: [
             { name: "alice", relationship: "Supervisor at DesignLab Solutions", contactNo: "0923-456-7890" },
